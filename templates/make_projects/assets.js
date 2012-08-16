47d3168fcd3b43c1f7e216ca3f2c8f991e14df0d
Nellie McKesson <nellie@oreilly.com>
2012-08-16 16:59:31 -0400

    more atempts at javascript

--- templates/make_projects/assets.js
+++ templates/make_projects/assets.js
@@ -1,14 +1,28 @@
-function init() {
 var script = document.createElement('script');
 script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-var myLocation = $("h2.title").first();
-var myTD = $("div.navheader table tr td");
-document.getElementById("whenever_you_ad").innerHTML = myLocation.html();
-myTD.innerHTML = ": " + myLocation.html();
-$("div.navheader table tr:first-of-type td").append( ": ", myLocation.html() );
+function init() {
+
+var elem = $(".sect1");
+
+$(".sect1").each(function() {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(this).offset().top;
+    var elemBottom = elemTop + $(this).height();
+
+    var isVisible = ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    if  isVisible = true {
+    var myLocation = $(this).children("div.titlepage div div h2.title");
+    $("div.navheader table tr:first-of-type td").append( ": ", myLocation.html() );
+    return false;
+    }
+    
+});
+
 }
 
 window.onload = init;
