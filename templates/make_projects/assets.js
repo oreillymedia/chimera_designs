94a9bb69ce239b4151c790f8c167f6a529209cee
Nellie McKesson <nellie@oreilly.com>
2012-08-16 15:34:53 -0400

    more atempts at javascript

--- templates/make_projects/assets.js
+++ templates/make_projects/assets.js
@@ -5,9 +5,9 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 var myLocation = $("h2.title").first();
-var myTD = $("div.navheader table tr:first-of-type td");
+var myTD = $("div.navheader table tr:first-of-type td:after");
 document.getElementById("whenever_you_ad").innerHTML = myLocation.html();
-myTD.innerHTML = myLocation.html();
+myTD.innerHTML = ": " + myLocation.html();
 }
 
 window.onload = init;
