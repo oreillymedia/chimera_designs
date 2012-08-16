60c02fe723bf2e166cb38816c3fa7f5c8d344a0f
Nellie McKesson <nellie@oreilly.com>
2012-08-16 14:35:25 -0400

    more atempts at javascript

--- templates/make_projects/assets.js
+++ templates/make_projects/assets.js
@@ -1,4 +1,4 @@
 var myPath = window.location;
-var myPathArray = myPath.split("#");
+var myPathArray = myPath.split("/");
 var myLocation = myPathArray.shift();
 document.getElementsByClassName("navheader").getElementsByTagName("table")[0].getElementsByTagName("tr")[0].getElementsByTagName("td")[0].innerHTML=myLocation;
