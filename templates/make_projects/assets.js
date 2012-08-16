6ccd31f7b547858b143c7a984ca3608c2c3e66f0
Nellie McKesson <nellie@oreilly.com>
2012-08-16 13:58:40 -0400

    more atempts at javascript

--- templates/make_projects/assets.js
+++ templates/make_projects/assets.js
@@ -1,2 +1,2 @@
-var runHead = document.getElementByTagName("h2").first();
+var runHead = document.getElementByTagName("h2") [0];
 document.getElementByClassName("navheader").getElementByTagName("table").getElementByTagName("tr").first().innerHTML=runHead;
