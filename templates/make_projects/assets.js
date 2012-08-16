ef2c5d418bc6000ba0f62572adf0da60f78d5f41
Nellie McKesson <nellie@oreilly.com>
2012-08-16 15:13:01 -0400

    more atempts at javascript

--- templates/make_projects/assets.js
+++ templates/make_projects/assets.js
@@ -3,6 +3,6 @@ script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-var myLocation = $("h2.title");
+var myLocation = $("h2.title").first();
 var myTD = $("div.navheader table tr:first-of-type td");
 myTD.innerHTML = myLocation;
