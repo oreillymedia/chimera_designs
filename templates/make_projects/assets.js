fa0b24e002397b1b60a91f74ba5e2f85466bd1ee
Rune Skjoldborg Madsen <rune@runemadsen.com>
2012-08-16 21:30:38 -0300

    Update templates/make_projects/assets.js
    Rune updating JS

--- templates/make_projects/assets.js
+++ templates/make_projects/assets.js
@@ -1,26 +1,21 @@
-var script = document.createElement('script');
-script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+$(function() {
+	
+	var headings = $("h2.title").toArray().reverse();
+	var currentHeading = null;
+	
+	$(document).scroll(function() {
+		
+		var window_top = $(document).scrollTop();
 
-function init() {
+		var newHeading = _.find(headings, function(header) {
+				return window_top >= $(header).position().top;
+		});
+		
+		if(newHeading !== currentHeading)
+		{
+			$("div.navheader table tr:first td").html($(newHeading).text());
+		}
 
-$("body").scroll(function() {
-	$(".sect1").each(function() {
-    	var docViewTop = $(window).scrollTop();
-    	var docViewBottom = docViewTop + $(window).height();
-
-   		var elemTop = $(this).offset().top;
-    	var elemBottom = elemTop + $(this).height();
-
-    	if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
-    		var myLocation = $(this).find("h2.title");
-    		$("div.navheader table tr:first-of-type td").append( ": ", myLocation.html() );
-    	};
-    
 	});
+	
 });
-
-}
-
-window.onload = init;
