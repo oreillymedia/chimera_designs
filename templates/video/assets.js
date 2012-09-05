420e91fbe476cf65ddc221f539e020b13efabf03
Adam Witwer <adam@oreilly.com>
2012-09-05 18:44:46 -0400

    rewriting JS

--- templates/video/assets.js
+++ templates/video/assets.js
@@ -1,15 +1,12 @@
  $(document).ready(function(){
         $(".turnoff").click(function () {
-        $(".lightsout").show();
-        $(".lightsout").css({'background-color' : '#000'});
+        $(".lightsout").css({'background-color' : '#000','display' : 'block'});
         });
       $(".soft").click(function () {
-        $(".lightsout").show();
-        $(".lightsout").css({'background-color' : '#4F4F4F'});
+        $(".lightsout").css({'background-color' : '#4F4F4F','display' : 'block'});
         });
       $(".turnon").click(function () {
-        $(".lightsout").css({'background-color' : 'transparent'});
-        $(".lightsout").hide();
+        $(".lightsout").css({'background-color' : 'transparent','display' : 'none'});
       });
     });
 
