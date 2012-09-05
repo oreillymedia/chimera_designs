b1b66afeb45161fe9aa4c2b9f83a6bf66cf7816c
Adam Witwer <adam@oreilly.com>
2012-09-05 18:25:05 -0400

    rewriting JS

--- templates/video/assets.js
+++ templates/video/assets.js
@@ -1,24 +1,18 @@
  $(document).ready(function(){
-      
-      $("#lightsout").fadeTo(1,0);
-      document.getElementById("lightsout").style.display="none";
-
-      $("#turnoff").click(function () {
-        $("#lightsout").css({'display' : 'block'});
-        $("#lightsout").fadeTo("slow",1);
-        $(".informalfigure").fadeTo("slow",1);
+        $(".turnoff").click(function () {
+        $("body").css({'background-color' : 'black'});
+        $("body").fadeTo("slow",1);
         });
-      $("#soft").click(function () {
-        document.getElementById("lightsout").style.display="block";
-        $("#lightsout").fadeTo("slow",0.8);
-        $(".informalfigure").fadeTo("slow",0.8);
+      $(".soft").click(function () {
+        $("body").css({'background-color' : 'black'});
+        $("body").fadeTo("slow",0.8);
         });
-      $("#turnon").click(function () {
-        document.getElementById("lightsout").style.display="block";
-        $("#lightsout").fadeTo("slow",0,function() {
-        document.getElementById("lightsout").style.display="none";
-        });
-      $(".informalfigure").fadeTo("slow",1);
+      $(".turnon").click(function () {
+        $("body").css({'background-color' : 'transparent'});
+        $("body").fadeTo("slow",0);
       });
     });
+
+
+
 
