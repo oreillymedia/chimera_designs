dfcf576c78ec1ff5acd97565619e211ff9c747d7
Adam Witwer <adam@oreilly.com>
2012-09-05 22:31:49 -0400

    rewriting JS

--- templates/video/assets.js
+++ templates/video/assets.js
@@ -1,5 +1,6 @@
  $(document).ready(function(){
-      $('<ul class="itemizedlist">\
+      $('<p class="lightsout" />\
+         <ul class="itemizedlist">\
            <li class="listitem"><p id="lights_out" class="turnoff">Lights Out</p></li>\
            <li class="listitem"><p id="soft_light" class="soft">Soft Light</p></li>\
            <li class="listitem"><p id="lights_on" class="turnon">Lights On</p></li>\
