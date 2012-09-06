8129ff6617648b6fcec4bc02e491ab61ce0bda1a
Adam Witwer <adam@oreilly.com>
2012-09-06 09:08:42 -0400

    rewriting fade effect

--- templates/video/assets.js
+++ templates/video/assets.js
@@ -7,10 +7,10 @@
          </ul>').insertBefore('.informalfigure');
       
       $(".turnoff").click(function () {
-        $(".lightsout").css({'display' : 'block', 'background-color' : '#000'});
+        $(".lightsout").css({'display' : 'block', 'background-color' : '#000', 'opacity' : '1'});
         });
       $(".soft").click(function () {
-        $(".lightsout").css({'display' : 'block', 'background-color' : '#000', 'opacity' : '0.8'});
+        $(".lightsout").css({'display' : 'block', 'background-color' : '#000', 'opacity' : '0.9'});
         });
       $(".turnon").click(function () {
         $(".lightsout").css({'display' : 'none', 'background-color' : 'transparent'});
