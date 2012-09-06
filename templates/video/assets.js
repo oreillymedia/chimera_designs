7a1d3afd10978c4ab3ca014bdb77fea58b9d69e4
Adam Witwer <adam@oreilly.com>
2012-09-05 22:26:57 -0400

    rewriting JS

--- templates/video/assets.js
+++ templates/video/assets.js
@@ -1,5 +1,11 @@
  $(document).ready(function(){
-        $(".turnoff").click(function () {
+      $('<ul class="itemizedlist">\
+           <li class="listitem"><p id="lights_out" class="turnoff">Lights Out</p></li>\
+           <li class="listitem"><p id="soft_light" class="soft">Soft Light</p></li>\
+           <li class="listitem"><p id="lights_on" class="turnon">Lights On</p></li>\
+         </ul>').insertAfter('.lightsout');
+      
+      $(".turnoff").click(function () {
         $(".lightsout").css({'display' : 'block','background-color' : '#000'});
         });
       $(".soft").click(function () {
