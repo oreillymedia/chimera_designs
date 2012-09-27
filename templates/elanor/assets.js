d775c86761e5982a855d1dc05df46353d1eb3692
Nellie McKesson <nellie@oreilly.com>
2012-09-27 12:18:21 -0400

    attempting new template to circumvent weirdness

---
+++ templates/elanor/assets.js
@@ -0,0 +1,84 @@
+$(function() {
+	// hide the legalnotices
+	$(".titlepage .copyright").click(function() {
+		$(".titlepage .legalnotice").toggle();
+	});
+});
+
+$(function() {
+$(".coderunner .ulink").attr("target","_self");
+});
+
+$(function() {
+	$('.style1link a.ulink').click(function() {
+		$('.button1').css('transition-property','width');
+		$('.button1').css('-moz-transition-property','width');
+		$('.button1').css('-webkit-transition-property','width');
+		$('.button1').css('-o-transition-property','width');
+	});
+});
+
+$(function() {
+	$('.style2link a.ulink').click(function() {
+		$('.button1').css('transition-property','background-color');
+		$('.button1').css('-moz-transition-property','background-color');
+		$('.button1').css('-webkit-transition-property','background-color');
+		$('.button1').css('-o-transition-property','background-color');
+	});
+});
+
+$(function() {
+	$('.style3link a.ulink').click(function() {
+		$('.button1').css('transition-duration','2s');
+		$('.button1').css('-moz-transition-duration','2s');
+		$('.button1').css('-webkit-transition-duration','2s');
+		$('.button1').css('-o-transition-duration','2s');
+	});
+});
+
+$(function() {
+	$('.style4link a.ulink').click(function() {
+		$('.button1').css('transition-duration','4s');
+		$('.button1').css('-moz-transition-duration','4s');
+		$('.button1').css('-webkit-transition-duration','4s');
+		$('.button1').css('-o-transition-duration','4s');
+	});
+});
+
+$(function() {
+	$('.style5link a.ulink').click(function() {
+		$('.button1').css('transition-timing-function','ease-out');
+		$('.button1').css('-moz-transition-timing-function','ease-out');
+		$('.button1').css('-webkit-transition-timing-function','ease-out');
+		$('.button1').css('-o-transition-timing-function','ease-out');
+	});
+});
+
+$(function() {
+	$('.style6link a.ulink').click(function() {
+		$('.button1').css('transition-timing-function','ease-in-out');
+		$('.button1').css('-moz-transition-timing-function','ease-in-out');
+		$('.button1').css('-webkit-transition-timing-function','ease-in-out');
+		$('.button1').css('-o-transition-timing-function','ease-in-out');
+	});
+});
+
+$(function() {
+	$('.style4link a.ulink').click(function() {
+		$('.button1').css('transition-delay','2s');
+		$('.button1').css('-moz-transition-delay','2s');
+		$('.button1').css('-webkit-transition-delay','2s');
+		$('.button1').css('-o-transition-delay','2s');
+	});
+});
+
+$(function() {
+	$('.style4link a.ulink').click(function() {
+		$('.button1').css('transition-delay','4s');
+		$('.button1').css('-moz-transition-delay','4s');
+		$('.button1').css('-webkit-transition-delay','4s');
+		$('.button1').css('-o-transition-delay','4s');
+	});
+});
+
+
